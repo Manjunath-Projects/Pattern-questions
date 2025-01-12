7bf0fdfd4ad879f89e357bb9d1b63269460925a7
MANJUNATH PATIL <155898796+Manjunath-Projects@users.noreply.github.com>
2025-01-12 13:39:03 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -2,7 +2,7 @@ public class Pattern2{
 	public static void main(String[] args){
 		int n=4;
 		for(int i=1;i<=n;i++){
-		for(int j=4;j>=i;j--){
+		for(int j=n;j>=i;j--){
 			System.out.print(" * ");
 		}
 		System.out.println();
